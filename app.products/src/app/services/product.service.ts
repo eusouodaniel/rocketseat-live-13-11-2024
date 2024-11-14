66ccdbe57e14f563f65a7168494cd0fda283909b
eusouodaniel <danielrodriguesdrs331@gmail.com>
2024-11-13 21:40:14 -0300

    new: add k8s manifest

--- app.products/src/app/services/product.service.ts
+++ app.products/src/app/services/product.service.ts
@@ -7,6 +7,8 @@ export class ProductService {
     return [
       { name: 'Produto 1', description: 'Produto 1 descrição' },
       { name: 'Produto 2', description: 'Produto 2 descrição' },
+      { name: 'Produto 3', description: 'Produto 3 descrição' },
+      { name: 'Produto 4', description: 'Produto 4 descrição' },
     ];
   }
 }
